5a725409f1e8d2c3166e8eba62c4b6818c92f19c
Lerish-vZ <lerishavz@gmail.com>
2023-09-12 09:07:25 +0200

    Update

--- movie-reviews/backend/dao/moviesDAO.js
+++ movie-reviews/backend/dao/moviesDAO.js
@@ -1,15 +1,13 @@
-let movies
-export default class MoviesDAO{
-static async injectDB(conn){
-if(movies){
-return
-}
-try{
-movies = await conn.db(process.env.MOVIEREVIEWS_NS)
-.collection('movies')
-}
-catch(e){
-console.error(`unable to connect in MoviesDAO: ${e}`)
-}
-}
+let movies;
+export default class MoviesDAO {
+  static async injectDB(conn) {
+    if (movies) {
+      return;
+    }
+    try {
+      movies = await conn.db(process.env.MOVIEREVIEWS_NS).collection("movies");
+    } catch (e) {
+      console.error(`unable to connect in MoviesDAO: ${e}`);
+    }
+  }
 }
