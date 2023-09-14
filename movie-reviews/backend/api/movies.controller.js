97cd32d12f9801abe8b7d4167cd53f1abe64e7ae
Lerish-vZ <lerishavz@gmail.com>
2023-09-14 12:10:17 +0200

    Update

--- movie-reviews/backend/api/movies.controller.js
+++ movie-reviews/backend/api/movies.controller.js
@@ -8,5 +8,23 @@ export default class MoviesController{
         const page = req.query.page ? parseInt(req.query.page) : 0
 
         let filters = {}
+        if(req.query.rated){
+            filters.rated = req.query.rated
+        }
+        else if(req.query.title){
+            filters.title = req.query.title
+        }
+
+        const { moviesList, totalNumMovies } = await 
+        MoviesDAO.getMovies({filters, page, moviesPerPage})
+
+        let response = {
+            movies: moviesList, 
+            page: page,
+            filters: filters, 
+            entries_per_page: moviesPerPage,
+            total_results: totalNumMovies,
+        }
+        res.json(response)
     }
 }
