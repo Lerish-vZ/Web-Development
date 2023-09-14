c2dc7e102f00b9c1a4a8f7516e0067d7cccfd08a
Lerish-vZ <lerishavz@gmail.com>
2023-09-14 12:21:30 +0200

    Update

--- movie-reviews/backend/api/movies.route.js
+++ movie-reviews/backend/api/movies.route.js
@@ -3,6 +3,6 @@ import MoviesController from './movies.controller.js';
 
 const router = express.Router(); // get access to express router
 
-router.route("/").get(MoviesController.apiGetMovies);
+router.route("/").get((req,res) => res.send('hello world'))//get(MoviesController.apiGetMovies);
 
 export default router;
