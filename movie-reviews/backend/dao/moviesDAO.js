7a5a52352fd4643091cf9254ff5c2db39f8b4931
Lerish-vZ <lerishavz@gmail.com>
2023-09-14 11:23:23 +0200

    Update

--- movie-reviews/backend/dao/moviesDAO.js
+++ movie-reviews/backend/dao/moviesDAO.js
@@ -13,31 +13,32 @@ export default class MoviesDAO {
       console.error(`unable to connect in MoviesDAO: ${e}`);
     }
   }
-  static async getMovies({// default filter
+  static async getMovies({
+    // default filter
     filters = null,
     page = 0,
     moviesPerPage = 20, // will only get 20 movies at once
-    } = {}){
-    let query
-    if(filters){
-    if("title" in filters){
-    query = { $text: { $search: filters['title']}}
-    }else if("rated" in filters){
-    query = { "rated": { $eq: filters['rated']}}
+  } = {}) {
+    let query;
+    if (filters) {
+      if ("title" in filters) {
+        query = { $text: { $search: filters["title"] } };
+      } else if ("rated" in filters) {
+        query = { rated: { $eq: filters["rated"] } };
+      }
     }
+    let cursor;
+    try {
+      cursor = await movies
+        .find(query)
+        .limit(moviesPerPage)
+        .skip(moviesPerPage * page);
+      const moviesList = await cursor.toArray();
+      const totalNumMovies = await movies.countDocuments(query);
+      return { moviesList, totalNumMovies };
+    } catch (e) {
+      console.error(`Unable to issue find command, ${e}`);
+      return { moviesList: [], totalNumMovies: 0 };
     }
-    let cursor
-    try{
-    cursor = await movies
-    .find(query)
-    .limit(moviesPerPage)
-.skip(moviesPerPage * page)
-const moviesList = await cursor.toArray()
-const totalNumMovies = await movies.countDocuments(query)
-return {moviesList, totalNumMovies}
-}
-catch(e){
-console.error(`Unable to issue find command, ${e}`)
-return { moviesList: [], totalNumMovies: 0}
-}
+  }
 }
