a57f082daf7522a520706a2a6380a60bc3a51f14
Lerish-vZ <lerishavz@gmail.com>
2023-09-14 11:48:13 +0200

    Update

--- movie-reviews/backend/api/movies.controller.js
+++ movie-reviews/backend/api/movies.controller.js
@@ -1,3 +1,12 @@
 import MoviesDAO from '../dao/moviesDAO.js';
 
-export default class MoviesController
+export default class MoviesController{
+
+    static async apiGetMovies(req, res, next) {
+        const moviesPerPage = req.query.moviesPerPage ?
+        parseInt(req.query.moviesPerPage) : 20
+        const page = req.query.page ? parseInt(req.query.page) : 0
+
+        let filters = {}
+    }
+}
