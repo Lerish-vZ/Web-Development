735695df0e75b1bf6a15c16130ae35f98f76b237
Lerish-vZ <lerishavz@gmail.com>
2023-10-12 09:46:19 +0200

    Update

--- Chapter_15/movie-reviews/frontend/src/App.js
+++ Chapter_15/movie-reviews/frontend/src/App.js
@@ -13,7 +13,7 @@ import Login from "./components/login";
 
 function App() {
   //react.useState is a 'hook'that lets us add some local state to functional components
-   const [user, setUser] = React.useState(null);
+  const [user, setUser] = React.useState(null);
 
   //  useState returns an array with two values: the current state value and a function that lets you update it
 
@@ -40,13 +40,32 @@ function App() {
               {/* ternary statement where if its true, execute the section after the '?' */}
               {/* If false, execute the section after the colon ':'. */}
               {/* if user is logged in (true) display logout, otherwise display login and link it to /login  */}
-              {user ? <a onClick={logout}> Logout User </a> : <Link to={"/login"}> Login </Link>}
+              {user ? (
+                <a onClick={logout}> Logout User </a>
+              ) : (
+                <Link to={"/login"}> Login </Link>
+              )}
 
               {/* In React, we can use curly braces ‘{}’ to put in code. */}
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+      <Switch>
+        <Route exact path={["/", "/movies"]} component={MoviesList}></Route>
+        <Route
+          path="/movies/:id/review"
+          render={(props) => <AddReview {...props} user={user} />}
+        ></Route>
+        <Route
+          path="/movies/:id/"
+          render={(props) => <Movie {...props} user={user} />}
+        ></Route>
+        <Route
+          path="/login"
+          render={(props) => <Login {...props} login={login} />}
+        ></Route>
+      </Switch>
     </div>
   );
 }
