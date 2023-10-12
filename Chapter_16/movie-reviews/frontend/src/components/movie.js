058c8109452ac7c6c001f209ad8a64c4c3a9e455
Lerish-vZ <lerishavz@gmail.com>
2023-10-12 10:17:16 +0200

    Update

---
+++ Chapter_16/movie-reviews/frontend/src/components/movie.js
@@ -0,0 +1,13 @@
+//component to list a single movie
+
+import React from 'react';
+
+function Movie() {
+    return (
+        <div className='App'>
+            Movie
+        </div>
+    );
+}
+
+export default Movie;
---
+++ Chapter_16/movie-reviews/frontend/src/components/movies-list.js
@@ -0,0 +1,13 @@
+//component to list movies
+
+import React from 'react';
+
+function MoviesList() {
+    return (
+        <div className='App'>
+            Movies List
+        </div>
+    );
+}
+
+export default MoviesList;
