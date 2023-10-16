0be000e0055369f80ed4eaa1f3ddd0d1a136050a
Lerish-vZ <lerishavz@gmail.com>
2023-10-16 14:56:33 +0200

    Update

--- Chapter_17/movie-reviews/frontend/src/components/movies-list.js
+++ Chapter_17/movie-reviews/frontend/src/components/movies-list.js
@@ -4,7 +4,8 @@ import React, { useState, useEffect } from "react"; //import useState to create 
 import MovieDataService from "../services/movies";
 import { Link } from "react-router-dom";
 
-const MoviesList = (props) => { // is functional component and receives and uses props
+const MoviesList = (props) => {
+  // is functional component and receives and uses props
   const [movies, setMovies] = useState([]); //use the React useState hook to create the movies, searchTitle, searchRating and ratings state variables.
   const [searchTitle, setSearchTitle] = useState("");
   const [searchRating, setSearchRating] = useState("");
@@ -12,31 +13,31 @@ const MoviesList = (props) => { // is functional component and receives and uses
 };
 
 useEffect(() => {
-    retrieveMovies()
-    retrieveRatings()
-}, [])
+  retrieveMovies();
+  retrieveRatings();
+}, []);
 
 const retrieveMovies = () => {
-    MovieDataService.getAll()
-    .then(response => {
-        console.log(response.data.movies)
+  MovieDataService.getAll()
+    .then((response) => {
+      console.log(response.data.movies);
     })
-    .catch( e => {
-        console.log(e)
-    })
-}
+    .catch((e) => {
+      console.log(e);
+    });
+};
 
 const retrieveRatings = () => {
-    MovieDataService.getRatings()
-    .then(response => {
-        console.log(response.data)
-        //start with 'All ratings' if user doesn't specify any ratings
-        setRatings(["All Ratings"].concat(response.data))
+  MovieDataService.getRatings()
+    .then((response) => {
+      console.log(response.data);
+      //start with 'All ratings' if user doesn't specify any ratings
+      setRatings(["All Ratings"].concat(response.data));
     })
-    .catch(e => {
-        console.log(e)
-    })
-}
+    .catch((e) => {
+      console.log(e);
+    });
+};
 
 function MoviesList() {
   return <div className="App">Movies List</div>;
