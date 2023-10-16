acecf645f8e2c95937c3abcb8ba14cf33fcd38ae
Lerish-vZ <lerishavz@gmail.com>
2023-10-16 14:50:05 +0200

    Update

--- Chapter_17/movie-reviews/frontend/src/components/movies-list.js
+++ Chapter_17/movie-reviews/frontend/src/components/movies-list.js
@@ -1,14 +1,18 @@
 //component to list movies
 
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import MovieDataService from "../services/movies";
+import { Link } from "react-router-dom";
 
+const MoviesList = (props) => {
+  const [movies, setMovies] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+  const [searchRating, setSearchRating] = useState("");
+  const [ratings, setRatings] = useState(["All Ratings"]);
+};
 
 function MoviesList() {
-    return (
-        <div className='App'>
-            Movies List
-        </div>
-    );
+  return <div className="App">Movies List</div>;
 }
 
 export default MoviesList;
