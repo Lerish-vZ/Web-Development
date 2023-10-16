7acfd060caa81b1ff12286541d3a3d699b351a78
Lerish-vZ <lerishavz@gmail.com>
2023-10-16 15:00:03 +0200

    Update

--- Chapter_17/movie-reviews/frontend/src/components/movies-list.js
+++ Chapter_17/movie-reviews/frontend/src/components/movies-list.js
@@ -10,37 +10,49 @@ const MoviesList = (props) => {
   const [searchTitle, setSearchTitle] = useState("");
   const [searchRating, setSearchRating] = useState("");
   const [ratings, setRatings] = useState(["All Ratings"]);
+
+  useEffect(() => {
+    retrieveMovies();
+    retrieveRatings();
+  }, []);
+
+  const retrieveMovies = () => {
+    MovieDataService.getAll()
+      .then((response) => {
+        console.log(response.data.movies);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const retrieveRatings = () => {
+    MovieDataService.getRatings()
+      .then((response) => {
+        console.log(response.data);
+        //start with 'All ratings' if user doesn't specify any ratings
+        setRatings(["All Ratings"].concat(response.data));
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const onChangeSearchTitle = (e) => {
+    const searchTitle = e.target.value;
+    setSearchTitle(searchTitle);
+  };
+  const onChangeSearchRating = (e) => {
+    const searchRating = e.target.value;
+    setSearchRating(searchRating);
+  };
+  
 };
 
-useEffect(() => {
-  retrieveMovies();
-  retrieveRatings();
-}, []);
 
-const retrieveMovies = () => {
-  MovieDataService.getAll()
-    .then((response) => {
-      console.log(response.data.movies);
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-};
 
-const retrieveRatings = () => {
-  MovieDataService.getRatings()
-    .then((response) => {
-      console.log(response.data);
-      //start with 'All ratings' if user doesn't specify any ratings
-      setRatings(["All Ratings"].concat(response.data));
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-};
-
-function MoviesList() {
-  return <div className="App">Movies List</div>;
-}
+// function MoviesList() {
+//   return <div className="App">Movies List</div>;
+// }
 
 export default MoviesList;
