a0a2527bca62626281583ea973674ab3f1711711
Lerish-vZ <lerishavz@gmail.com>
2023-10-16 14:52:47 +0200

    Update

--- Chapter_17/movie-reviews/frontend/src/components/movies-list.js
+++ Chapter_17/movie-reviews/frontend/src/components/movies-list.js
@@ -1,15 +1,17 @@
 //component to list movies
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"; //import useState to create a series of state variables.
 import MovieDataService from "../services/movies";
 import { Link } from "react-router-dom";
 
-const MoviesList = (props) => {
-  const [movies, setMovies] = useState([]);
+const MoviesList = (props) => { // is functional component and receives and uses props
+  const [movies, setMovies] = useState([]); //use the React useState hook to create the movies, searchTitle, searchRating and ratings state variables.
   const [searchTitle, setSearchTitle] = useState("");
   const [searchRating, setSearchRating] = useState("");
   const [ratings, setRatings] = useState(["All Ratings"]);
 };
+
+
 
 function MoviesList() {
   return <div className="App">Movies List</div>;
