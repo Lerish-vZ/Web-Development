601e1267918f5d5de181d6b56fa1d31f6ded2d56
Lerish-vZ <lerishavz@gmail.com>
2024-01-01 15:58:53 +0200

    Update

--- Chapter_18/movie-reviews/frontend/src/components/movie.js
+++ Chapter_18/movie-reviews/frontend/src/components/movie.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
 import MovieDataService from "../services/movies";
 import { Link } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Media from 'react-bootstrap/Media';
+
 const Movie = (props) => {
   const [movie, setMovie] = useState({
     id: null,
@@ -8,6 +16,7 @@ const Movie = (props) => {
     rated: "",
     reviews: [],
   });
+
   const getMovie = (id) => {
     MovieDataService.get(id)
       .then((response) => {
@@ -18,8 +27,10 @@ const Movie = (props) => {
         console.log(e);
       });
   };
+
   useEffect(() => {
     getMovie(props.match.params.id);
   }, [props.match.params.id]);
+
   return <div></div>;
 };
