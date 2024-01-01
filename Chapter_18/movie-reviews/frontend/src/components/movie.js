ecbce759cf772b0460b5a2019652c1e71d358c5d
Lerish-vZ <lerishavz@gmail.com>
2024-01-01 15:55:53 +0200

    Update

--- Chapter_18/movie-reviews/frontend/src/components/movie.js
+++ Chapter_18/movie-reviews/frontend/src/components/movie.js
@@ -1,13 +1,25 @@
-//component to list a single movie
-
-import React from 'react';
-
-function Movie() {
-    return (
-        <div className='App'>
-            Movie
-        </div>
-    );
-}
-
-export default Movie;
+import React, { useState, useEffect } from "react";
+import MovieDataService from "../services/movies";
+import { Link } from "react-router-dom";
+const Movie = (props) => {
+  const [movie, setMovie] = useState({
+    id: null,
+    title: "",
+    rated: "",
+    reviews: [],
+  });
+  const getMovie = (id) => {
+    MovieDataService.get(id)
+      .then((response) => {
+        setMovie(response.data);
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  useEffect(() => {
+    getMovie(props.match.params.id);
+  }, [props.match.params.id]);
+  return <div></div>;
+};
